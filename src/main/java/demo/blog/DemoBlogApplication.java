3ff034d6aa3033723e08ebdaaa780c896532782b
unknown <ccshnue@gmail.com>
2019-09-07 09:36:41 +0700

    them chuc nang

--- src/main/java/demo/blog/DemoBlogApplication.java
+++ src/main/java/demo/blog/DemoBlogApplication.java
@@ -1,12 +1,19 @@
 package demo.blog;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoBlogApplication {
-
+	
 	public static void main(String[] args) {
+		String test = "19212345";
+		int a = Integer.valueOf(test.substring(0, test.length() - 5));
+		System.out.println(a);
+	
 		SpringApplication.run(DemoBlogApplication.class, args);
 	}
 
